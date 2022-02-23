9d5a6456b47dc640fd80862f09e15d0120944ce1
caxefaizan <63395276+caxefaizan@users.noreply.github.com>
2022-02-23 15:59:07 +0530

    Update my_dag.py

--- my_dag.py
+++ my_dag.py
@@ -20,7 +20,7 @@ with DAG(
     # [START howto_operator_bash]
     run_this = BashOperator(
         task_id='run_after_loop',
-        bash_command='echo 1',
+        bash_command='pwd',
     )
     # [END howto_operator_bash]
 
